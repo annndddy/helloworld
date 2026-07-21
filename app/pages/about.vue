eb07d479849ad0435ff92fb0e54c1b5ca1db4228
Andy Lam <andyptlam@ricacorp.com>
2026-07-21 15:04:52 +0800

    feat: more text on about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -7,6 +7,7 @@
       <UCardBody>
         <p class="text-gray-700 text-base">
           This is the about page of the Nuxt 4 project.
+          ADD TEXT
         </p>
       </UCardBody>
     </UCard>
